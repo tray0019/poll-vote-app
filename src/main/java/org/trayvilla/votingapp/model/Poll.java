e8358e10db97bb5ddf94ceebdc458a1ca416c469
tray0019 <tray0019@algonquinlive.com>
2025-08-15 10:02:38 -0400

    added a Poll model where I have added id,question field. I also added two @ElementCollection for options and votes. This will create separate table in the databse that will have id and question.

---
+++ src/main/java/org/trayvilla/votingapp/model/Poll.java
@@ -0,0 +1,28 @@
+package org.trayvilla.votingapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Poll {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String question;
+
+    @ElementCollection
+    private List<String> options = new ArrayList<>();
+
+    @ElementCollection
+    private List<String> votes = new ArrayList<>();
+
+//continue at 2:55
+
+}
