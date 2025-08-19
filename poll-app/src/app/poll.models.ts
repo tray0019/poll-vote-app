84b3afd360979f702cca449dabab2c2780842ced
tray0019 <tray0019@algonquinlive.com>
2025-08-19 10:30:20 -0400

    Added PollService class. This class connects my frontend and Backend. In my class I have set my baseUrl and constructor. I have created 3 methods for creating a poll, getting a poll and voting.

--- poll-app/src/app/poll.models.ts
+++ poll-app/src/app/poll.models.ts
@@ -1,2 +1,12 @@
+export interface OptionVote{
+    optionText: string
+    vote: number;
+}
+
 export interface Poll {
+
+    id: number;
+    question: string;
+    options: OptionVote[]; 
+
 }
--- poll-app/src/app/poll.ts
+++ poll-app/src/app/poll.ts
@@ -1,8 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Poll } from './poll.models';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class Poll {
-  
+@Injectable({providedIn: 'root'})
+export class PollService {
+  private baseUrl = 'http://localhost:8080/api/polls'
+
+  constructor(private http: HttpClient){
+
+    createPoll(poll: Poll)
+
+  }
 }
