85c47cd08d5c36913e57feb35f5b7d78d5df8588
tray0019 <tray0019@algonquinlive.com>
2025-08-20 11:52:43 -0400

    modified voteOption to optionText similar to my front end OptionVote class

--- src/main/java/org/trayvilla/votingapp/model/OptionVote.java
+++ src/main/java/org/trayvilla/votingapp/model/OptionVote.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Embeddable
 public class OptionVote {
-    private String voteOption;
+    private String optionText;
     private Long voteCount = 0L;
 }
