5aba5a8d49087515c82ecb3a6f9f5d230b41484d
tray0019 <tray0019@algonquinlive.com>
2025-08-20 10:53:58 -0400

    added provideHttpClient in my config to perform HttpRequest.

--- poll-app/src/app/poll/poll.ts
+++ poll-app/src/app/poll/poll.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PollService } from '../poll';
+import { Poll } from '../poll.models';
 
 @Component({
   selector: 'app-poll',
@@ -6,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './poll.html',
   styleUrl: './poll.css'
 })
-export class PollComponent {
+export class PollComponent implements OnInit {
+  polls: Poll[] = [];
+  constructor(private pollService: PollService){}
+
+  ngOnInit(): void {
+    this.loadPolls();
+  }
+
+  loadPolls(){
+    this.pollService.getPolls().subscribe({
+      next: (data) => {
+        this.polls = data;
+      },
+
+      error: (error) => {
+        console.error("Error fetching polls: ", error);
+      }
+    });
+  }
 
 }
