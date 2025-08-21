876bb998f64034b5f31c09c39b8ab32a183251cf
tray0019 <tray0019@algonquinlive.com>
2025-08-21 10:39:12 -0400

    Added Dockerfile

--- poll-app/src/app/poll.ts
+++ poll-app/src/app/poll.ts
@@ -2,25 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Poll } from './poll.models';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 
 @Injectable({providedIn: 'root'})
 export class PollService {
-  private baseUrl = 'http://localhost:8080/api/polls'
+  private baseUrl = `${environment.apiBaseUrl}/api/polls`;
 
   constructor(private http: HttpClient){ }
 
-  createPoll(poll: Poll): Observable<Poll>{
+  createPoll(poll: Poll){
     return this.http.post<Poll>(this.baseUrl, poll);
   }
 
-  getPolls(): Observable<Poll[]>{
+  getPolls(){
     return this.http.get<Poll[]>(this.baseUrl);
   }
 
-  vote(pollId: number, optionIndex: number): Observable<void> {
-    const url = `${this.baseUrl}/vote`;
-    return this.http.post<void>(url, {pollId, optionIndex});
+  vote(pollId: number, optionIndex: number){
+    return this.http.post<void>(`${this.baseUrl}/vote`, {pollId, optionIndex});
   }
 
 }
